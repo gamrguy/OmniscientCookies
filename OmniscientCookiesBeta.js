7f4c1b1ef3e867585ede7aa74131502bdfdf2a95
gamrguy <colmandavenport@gmail.com>
2020-12-06 22:35:17 -0500

    Update OmniscientCookiesBeta.js

--- OmniscientCookiesBeta.js
+++ OmniscientCookiesBeta.js
@@ -1,6 +1,6 @@
 OmniCookies = {
 	name: 'Omniscient Cookies',
-	version: 'v1.2.5 BETA 6'
+	version: 'v1.2.5 BETA 7'
 };
 
 OmniCookies.settings = {
@@ -23,6 +23,7 @@ OmniCookies.settings = {
 OmniCookies.saveData = {}
 OmniCookies.defaultSave = function() {
 	OmniCookies.saveData.stockAverages = [];
+	OmniCookies.frozenWrinks = null;
 }
 OmniCookies.defaultSave();
 

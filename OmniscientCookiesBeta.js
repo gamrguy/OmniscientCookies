3431fa19023c59105daf3c57a2fced5fa703a321
gamrguy <colmandavenport@gmail.com>
2020-12-07 00:31:56 -0500

    Update OmniscientCookiesBeta.js

--- OmniscientCookiesBeta.js
+++ OmniscientCookiesBeta.js
@@ -1,6 +1,6 @@
 OmniCookies = {
 	name: 'Omniscient Cookies',
-	version: 'v1.2.5 BETA 17'
+	version: 'v1.2.5 BETA 18'
 };
 
 OmniCookies.settings = {
@@ -506,7 +506,7 @@ OmniCookies.patchBuffTooltips = function() {
 	debuffType.func = OmniCookies.replaceCode(debuffType.func, [
 		{
 			pattern: /\(Math\.\w+\(pow\*100-100\)\)/,
-			replacement: `(Math.round((1-(1/pow))*100)`
+			replacement: `(Math.round((1-(1/pow))*100))`
 		}
 	]);
 }
